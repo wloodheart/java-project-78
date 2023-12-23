f0a86321253fb28d81df5c150c083bb80a6f3425
wlood <wloodheart@gmail.com>
2023-12-23 21:41:10 +0300

    added StringSchema, Validator, Tests

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/wloodheart/java-project-78/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/wloodheart/java-project-78/actions)
-[![Actions Status](https://github.com/wloodheart/java-project-78/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/wloodheart/java-project-78/actions)
-[![Maintainability](https://api.codeclimate.com/v1/badges/cf17667895cc3ba17683/maintainability)](https://codeclimate.com/github/wloodheart/java-project-78/maintainability)
+![Java CI](https://github.com/wloodheart/java-project-71/workflows/Java%20CI/badge.svg)[![Maintainability](https://api.codeclimate.com/v1/badges/cf17667895cc3ba17683/maintainability)](https://codeclimate.com/github/wloodheart/java-project-78/maintainability)
 [![Test Coverage](https://api.codeclimate.com/v1/badges/cf17667895cc3ba17683/test_coverage)](https://codeclimate.com/github/wloodheart/java-project-78/test_coverage)
---
+++ app/src/test/java/hexlet/code/StringSchemaTest.java
@@ -0,0 +1,61 @@
+package hexlet.code;
+
+import hexlet.code.schemas.StringSchema;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class StringSchemaTest {
+    Validator validator = new Validator();
+
+    @Test
+    void testIsValidNull() {
+        StringSchema schema = validator.string();
+
+        assertThat(schema.isValid(null)).isTrue();
+    }
+
+    @Test
+    void testIsValidEmpty() {
+        StringSchema schema = validator.string();
+
+        assertThat(schema.isValid("")).isTrue();
+    }
+
+    @Test
+    void testIsValidRequiredNull() {
+        StringSchema schema = validator.string();
+        schema.required();
+
+        assertThat(schema.isValid(null)).isFalse();
+    }
+
+    @Test
+    void testIsValidRequiredEmpty() {
+        StringSchema schema = validator.string();
+        schema.required();
+
+        assertThat(schema.isValid("")).isFalse();
+    }
+
+    @Test
+    void testIsValid() {
+        StringSchema schema = validator.string();
+
+        assertThat(schema.isValid("what does the fox say")).isTrue();
+    }
+
+    @Test
+    void testIsValidContains() {
+        StringSchema schema = validator.string();
+
+        assertThat(schema.contains("wh").isValid("what does the fox say")).isTrue();
+    }
+
+    @Test
+    void testIsValidContains1() {
+        StringSchema schema = validator.string();
+
+        assertThat(schema.contains("whatthe").isValid("what does the fox say")).isFalse();
+    }
+}

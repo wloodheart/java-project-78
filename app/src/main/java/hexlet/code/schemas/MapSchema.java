855b7765cb6768866a07fc2eebca4e1d5608f922
wlood <wloodheart@gmail.com>
2023-12-24 18:35:37 +0300

    code refactor

--- app/src/main/java/hexlet/code/schemas/MapSchema.java
+++ app/src/main/java/hexlet/code/schemas/MapSchema.java
@@ -3,7 +3,7 @@ package hexlet.code.schemas;
 import java.util.Map;
 import java.util.Objects;
 
-public class MapSchema extends BaseSchema {
+public final class MapSchema extends BaseSchema {
     public MapSchema shape(Map<String, BaseSchema> map) {
 
         map.keySet().forEach(key -> conditions.add(s ->

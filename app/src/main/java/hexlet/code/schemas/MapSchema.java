b982831456996d0ed8b93c1f58382f3c798f048f
wlood <wloodheart@gmail.com>
2023-12-24 14:00:17 +0300

    added shape method and tests

--- app/src/main/java/hexlet/code/schemas/MapSchema.java
+++ app/src/main/java/hexlet/code/schemas/MapSchema.java
@@ -4,6 +4,16 @@ import java.util.Map;
 import java.util.Objects;
 
 public class MapSchema extends BaseSchema {
+    public MapSchema shape(Map<String, BaseSchema> map) {
+
+        map.keySet().forEach(key -> {
+            conditions.add(s ->
+                    map.get(key).isValid(((Map<?, ?>) s).get(key)));
+        });
+
+        return this;
+    }
+
     public MapSchema required() {
         conditions.add(Objects::nonNull);
         conditions.add(e -> e instanceof Map<?, ?>);
